c2f4f0fb761cd096522df9156612470001404c1d
Biboswan Roy <biboswan98@gmail.com>
2018-08-07 19:38:15 +0530

    input cred

---
+++ server/settings.js
@@ -0,0 +1,6 @@
+export const cookieSecret = 'someRandomSecretValue';
+
+// Google oauth
+export const clientId = '130050407258-l4of9eo84cak5e4lo707cgicgiunq2ug.apps.googleusercontent.com';
+export const clientSecret = 'OumXXrB17yr_yHc29dXQsIQ1';
+export const redirectOrigin = 'http://localhost:3000';
