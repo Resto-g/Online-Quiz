b6da332d7cea69f960e61560ef64ec9c81583cca
Biboswan Roy <biboswan98@gmail.com>
2018-08-07 23:53:04 +0530

    Add heroku url

--- server/settings.js
+++ server/settings.js
@@ -3,4 +3,4 @@ export const cookieSecret = 'someRandomSecretValue';
 // Google oauth
 export const clientId = '130050407258-l4of9eo84cak5e4lo707cgicgiunq2ug.apps.googleusercontent.com';
 export const clientSecret = 'OumXXrB17yr_yHc29dXQsIQ1';
-export const redirectOrigin = 'http://bigwebquiz.com';
+export const redirectOrigin = 'https://radiant-fortress-76251.herokuapp.com';
