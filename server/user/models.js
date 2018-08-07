f1b6cf30f0d80e6c4cdc1b548aa764fdb6b631f3
Biboswan Roy <biboswan98@gmail.com>
2018-08-07 20:31:28 +0530

    Include a new id

--- server/user/models.js
+++ server/user/models.js
@@ -21,7 +21,8 @@ export const ADMIN_IDS = [
   '116237864387312784020', // Jake
   '102813120987797040209', // Paul Lewis
   '104714371990859295637', // thebigwebquiz
-  '111820256548303113275'  // surma
+  '111820256548303113275',  // surma
+  '114787160188858148562' //bibo
 ];
 
 let allowNaiveLogin = false;
