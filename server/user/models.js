3769dfa6c7745824e314f7ba311ae513cbc01ae1
Biboswan Roy <biboswan98@gmail.com>
2018-10-03 16:54:10 +0530

    Fix pushAll compatibility issue

--- server/user/models.js
+++ server/user/models.js
@@ -1,28 +1,28 @@
 /**
-*
-* Copyright 2016 Google Inc. All rights reserved.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-import mongoose from '../mongoose-db';
+ *
+ * Copyright 2016 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import mongoose from "../mongoose-db";
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 export const ADMIN_IDS = [
-  '116237864387312784020', // Jake
-  '102813120987797040209', // Paul Lewis
-  '104714371990859295637', // thebigwebquiz
-  '111820256548303113275',  // surma
-  '114787160188858148562' //bibo
+  "116237864387312784020", // Jake
+  "102813120987797040209", // Paul Lewis
+  "104714371990859295637", // thebigwebquiz
+  "111820256548303113275", // surma
+  "114787160188858148562" //bibo
 ];
 
 let allowNaiveLogin = false;
@@ -35,24 +35,37 @@ export function setNaiveLogin(val) {
   allowNaiveLogin = !!val;
 }
 
-const userSchema = mongoose.Schema({
-  googleId: {type: String, unique: true, required: true, index: true},
-  name: {type: String, required: true, default: "Unknown name"},
-  email: String,
-  avatarUrl: {type: String, required: true, default: "/static/images/ic_tag_faces_white_18px.svg"},
-  optIntoLeaderboard: {type: Boolean, required: true, default: true},
-  bannedFromLeaderboard: {type: Boolean, required: true, default: false},
-  // Optimisation. See `updateScore`.
-  score: {type: Number, default: 0, index: true},
-  answers: [
-    {
-      questionId: {type: ObjectId, required: true},
-      choices: [Number]
-    }
-  ]
-});
+const userSchema = mongoose.Schema(
+  {
+    googleId: { type: String, unique: true, required: true, index: true },
+    name: { type: String, required: true, default: "Unknown name" },
+    email: String,
+    avatarUrl: {
+      type: String,
+      required: true,
+      default: "/static/images/ic_tag_faces_white_18px.svg"
+    },
+    optIntoLeaderboard: { type: Boolean, required: true, default: true },
+    bannedFromLeaderboard: { type: Boolean, required: true, default: false },
+    // Optimisation. See `updateScore`.
+    score: { type: Number, default: 0, index: true },
+    answers: [
+      {
+        questionId: { type: ObjectId, required: true },
+        choices: [Number]
+      }
+    ]
+  },
+  {
+    usePushEach: true
+  }
+);
 
-userSchema.index({ optIntoLeaderboard: 1, bannedFromLeaderboard: 1, score: -1 });
+userSchema.index({
+  optIntoLeaderboard: 1,
+  bannedFromLeaderboard: 1,
+  score: -1
+});
 
 userSchema.methods.isAdmin = function() {
   return ADMIN_IDS.includes(this.googleId);
@@ -66,7 +79,9 @@ userSchema.statics.updateScores = function(questions) {
       for (const question of questions) {
         if (!question.scored) continue;
 
-        const userAnswer = user.answers.find(answer => question._id.equals(answer.questionId));
+        const userAnswer = user.answers.find(answer =>
+          question._id.equals(answer.questionId)
+        );
         if (!userAnswer) continue;
         const choices = userAnswer.choices;
 
@@ -76,8 +91,7 @@ userSchema.statics.updateScores = function(questions) {
               score++;
             }
           }
-        }
-        else {
+        } else {
           const correctIndex = question.answers.findIndex(a => a.correct);
           if (choices[0] == correctIndex) {
             score += 4;
@@ -92,4 +106,4 @@ userSchema.statics.updateScores = function(questions) {
   });
 };
 
-export const User = mongoose.model('User', userSchema);
+export const User = mongoose.model("User", userSchema);
